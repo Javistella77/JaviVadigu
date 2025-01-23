f2fd921130f77ff9e86c8a97e1067a79a2da8e4f
Javistella77 <javierstella96@gmail.com>
2025-01-23 14:31:07 -0300

    Update VadiguSQL.sql

--- VadiguSQL.sql
+++ VadiguSQL.sql
@@ -169,4 +169,13 @@ select * from INF_Configuracion where usuarioid = 4000
 --delete from INF_Configuracion where usuarioid = 4000
 
 
+--                                              Eliminar trigger 
+DISABLE TRIGGER TR_TipoAdmisionAud_upd ON TurnoEncabezado
 
+--                                             Verificar que quede desabilitado 
+y esto para verificar que quedo deshabilado:
+Select
+    name AS TriggerName,
+    is_disabled AS IsDisabled
+FROM sys.triggers
+WHERE name = 'TR_TipoAdmisionAud_upd';
