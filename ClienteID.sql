cce8f2112879eb5d14d07e1569148b990a76a6bf
Javistella77 <javierstella96@gmail.com>
2025-02-27 14:48:49 -0300

    Update ClienteID.sql

--- ClienteID.sql
+++ ClienteID.sql
@@ -1,4 +1,4 @@
 update PARAMETROSISTEMAVALOR set valor = '12' WHERE PARAMETROSISTEMAID LIKE 'clienteid'
 
-
+select valor from ParametroSistemaValor  where ParametroSistemaid like 'Clienteid'
 --En Valor debe ir el cienteID a actualizar
